148625ad81e321a8f1affeea661e23edb04a5177
Sergei Volodin <etoestja@yandex.ru>
2018-03-23 10:53:43 +0100

    Add pretty-print

--- competitive_learning_ex3.py
+++ competitive_learning_ex3.py
@@ -1,13 +1,39 @@
 import numpy as np
+
+# numpy arr -> string
+def np_str(arr):
+  if len(arr.shape) == 1:
+    return ', '.join([str(x) for x in arr])
+  else:
+    # column first order
+    return ';\t'.join(['col%d: [%s]' % (i, np_str(x)) for i, x in enumerate(arr.T)])
+
+# inputs (column vectors)
 x = np.array([[0,1.,0], [1, 0, 1], [0, 1, 1]]).T
-#order = [0,1,2,0,1,2]
-order = [0,1,0,1]
+
+# inputs order (column ids)
+order = [0,1,2,0,1,2]
+#order = [0,1,0,1]
+
+# initial weight vector (column vectors = neurons)
 w = np.array([[1.5,0,0.5], [0, 0.5, 1.5]]).T
+
+# learning rate
 eta = 0.5
 
+# going through all inputs
 for i in order:
+  # current input
   inp = x[:, i]
+
+  # neuron activations
   activation = w.T @ inp
+
+  # winner
   winner = np.argmax(activation)
+
+  # winner update rule
   w[:, winner] += eta * (inp - w[:, winner])
-  print('Input %d %s, activation %s, winner %d, weights %s' % (i, str(inp), str(activation), winner, str(w)))
+
+  # printing result
+  print('Input %d: %s,\tactivation %s,\twinner %d,\tweights %s' % (i, np_str(inp), np_str(activation), winner, np_str(w)))
